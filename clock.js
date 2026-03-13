36b7ddf778512cb2d8edd853c1d8eead491e9974
Yadhukrishnan Pk <yadhukrishnanpkotml02@gmail.com>
2026-03-13 16:38:43 +0530

    Created js file and the functions implemented. Task finished successfully

--- clock.js
+++ clock.js
@@ -1,6 +1,32 @@
- let section = document.querySelector('section'),
- icons = document.querySelector(".icons");
+let section = document.querySelector("section"),
+  icons = document.querySelector(".icons");
 
- icons.onclick =()=>{
-    section.classList.toggle("dark");
- }
+//Dark mode
+icons.onclick = () => {
+  section.classList.toggle("dark");
+};
+
+// Time functions
+
+setInterval(() => {
+  let date = new Date(),
+    hours = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+
+  //AM AND PM CALCULATION
+
+  let ampm;
+  ampm = hours < 12 ? "AM" : "PM";
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours == 0 ? (hours = 12) : hours;
+  //Adding zero
+  hours = hours < 10 ? "0" + hours : hours;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  document.querySelector(".hour_num").innerText = hours;
+  document.querySelector(".min_num").innerText = min;
+  document.querySelector(".sec_num").innerText = sec;
+  document.querySelector(".am_pm").innerText = ampm;
+}, 1000); //time interval 1000ms=1s
